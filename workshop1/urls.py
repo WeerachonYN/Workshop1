8d20fa33aa3bf54d0181ba24bdbcaf016797d699
WeerachonYN <chin_acmilan@hotmail.com>
2021-06-12 16:35:46 +0700

    set up project

--- workshop1/urls.py
+++ workshop1/urls.py
@@ -18,7 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
+from shop.views.homepage import homepage
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+    # screen
+    path('',homepage,name = 'home'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
